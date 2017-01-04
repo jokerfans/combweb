0824950f331fe83315b9c7205392dea590cba5d9
tengxiang <1571903167@qq.com>
2017-01-04 23:12:29 +0800

    sql script

---
+++ src/main/webapp/WEB-INF/sql/demo20170104.sql
@@ -0,0 +1,67 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.1.73-community : Database - demo
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`demo` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `demo`;
+
+/*Table structure for table `course_info` */
+
+DROP TABLE IF EXISTS `course_info`;
+
+CREATE TABLE `course_info` (
+  `id` int(11) NOT NULL,
+  `cname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `caddress` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `course_info` */
+
+/*Table structure for table `course_user_info` */
+
+DROP TABLE IF EXISTS `course_user_info`;
+
+CREATE TABLE `course_user_info` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `uid` (`uid`),
+  KEY `cid` (`cid`),
+  CONSTRAINT `cid` FOREIGN KEY (`cid`) REFERENCES `course_info` (`id`),
+  CONSTRAINT `uid` FOREIGN KEY (`uid`) REFERENCES `user_info` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `course_user_info` */
+
+/*Table structure for table `user_info` */
+
+DROP TABLE IF EXISTS `user_info`;
+
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL,
+  `uname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `unumber` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `user_info` */
+
+insert  into `user_info`(`id`,`uname`,`unumber`) values (1,'xiaoming',4);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
